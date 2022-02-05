7dfb20f07e8cc3852be3baa2bc5acfbdb2d32774
sujin <rkdthd1234@naver.com>
2022-02-06 04:41:19 +0900

    챕터 1용 프로젝트 생성

---
+++ 1장/Chapter1/Chapter1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter1
+//
+//  Created by 강수진 on 2022/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 1장/Chapter1/Chapter1Tests/Chapter1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Chapter1Tests.swift
+//  Chapter1Tests
+//
+//  Created by 강수진 on 2022/02/06.
+//
+
+import XCTest
+@testable import Chapter1
+
+class Chapter1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
