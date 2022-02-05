e246e3e0fd3cdc0130a80d5c0bad5945ad9d106e
sujin <rkdthd1234@naver.com>
2022-02-06 03:04:41 +0900

    statement 함수 쪼개기

--- Refactoring.playground/Pages/1. Refactoring, a First Example.xcplaygroundpage/Sources/Statement.swift
+++ Refactoring.playground/Pages/1. Refactoring, a First Example.xcplaygroundpage/Sources/Statement.swift
@@ -7,23 +7,7 @@ public func statement(invoice: Invoice, plays: [String: Play]) throws -> String 
 
     for perf in invoice.performances {
         let play = plays[perf.playID]
-        var thisAmount: Double = 0
-
-        switch play?.type {
-        case "tragedy":
-            thisAmount = 40000
-            if perf.audience > 30 {
-                thisAmount += Double(1000 * (perf.audience - 30))
-            }
-        case "comedy":
-            thisAmount = 30000
-            if perf.audience > 20 {
-                thisAmount += Double(10000 + 500 * (perf.audience - 20))
-            }
-            thisAmount += Double(300 * perf.audience)
-        default:
-            throw CustomError.unknown
-        }
+        let thisAmount = try amountFor(perf: perf, play: play)
 
         volumeCredits += max(Double(perf.audience) - 30, 0)
         if "comedy" == play?.type {
@@ -36,4 +20,26 @@ public func statement(invoice: Invoice, plays: [String: Play]) throws -> String 
     result += "총액: $\(totalAmount/100.0)\n"
     result += "적립 포인트: $\(volumeCredits)점\n"
     return result
+    
+    func amountFor(performance: Performance, play: Play?) throws -> Double {
+        var thisAmount: Double = 0
+        
+        switch play?.type {
+        case "tragedy":
+            thisAmount = 40000
+            if performance.audience > 30 {
+                thisAmount += Double(1000 * (performance.audience - 30))
+            }
+        case "comedy":
+            thisAmount = 30000
+            if performance.audience > 20 {
+                thisAmount += Double(10000 + 500 * (performance.audience - 20))
+            }
+            thisAmount += Double(300 * performance.audience)
+        default:
+            throw CustomError.unknown
+        }
+        
+        return thisAmount
+    }
 }
