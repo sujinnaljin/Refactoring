27cf7012488462d935917e16064a963634f4993a
sujin <rkdthd1234@naver.com>
2022-02-26 04:36:40 +0900

    목업 데이터 불러올때 nil 이 아니도록 방어 테스트코드 추가 (nil 이면 디코딩 에러임을 알고 오류를 빨리 캐치할 수 있게)

--- Chapter4/Chapter4Tests/Chapter4Tests.swift
+++ Chapter4/Chapter4Tests/Chapter4Tests.swift
@@ -15,52 +15,54 @@ class Chapter4Tests: QuickSpec {
         
         // 어떤 component를 test 하는지 설명 (명사)
         describe("province") {
-            var asia: Province?
+            var asia: Province!
             
             beforeEach {
                 asia = MockParser.load(type: Province.self, fileName: "mock")
+                expect(asia).toNot(beNil())
             }
             // test에서 기대되는 결과. 위에서 명사로 작성한 테스트 대상의 행동을 작성
             it("shortfall") {
-                expect(asia?.shortfall).to(equal(5))
+                expect(asia.shortfall).to(equal(5))
             }
             
             it("profit") {
-                expect(asia?.profit).to(equal(230))
+                expect(asia.profit).to(equal(230))
             }
             
             it("change production") {
-                asia?.producers.first?.setProduction(amountStr: "20")
-                expect(asia?.shortfall).to(equal(-6))
-                expect(asia?.profit).to(equal(292))
+                asia.producers.first?.setProduction(amountStr: "20")
+                expect(asia.shortfall).to(equal(-6))
+                expect(asia.profit).to(equal(292))
             }
             
             it("zero demand") {
-                asia?.demand = 0
-                expect(asia?.shortfall).to(equal(-25))
-                expect(asia?.profit).to(equal(0))
+                asia.demand = 0
+                expect(asia.shortfall).to(equal(-25))
+                expect(asia.profit).to(equal(0))
             }
             
             it("negative demand") {
-                asia?.demand = -1
-                expect(asia?.shortfall).to(equal(-26))
-                expect(asia?.profit).to(equal(-10))
+                asia.demand = -1
+                expect(asia.shortfall).to(equal(-26))
+                expect(asia.profit).to(equal(-10))
             }
         }
         
         describe("no producers") {
-            var noProducers: Province?
+            var noProducers: Province!
             
             beforeEach {
                 noProducers = MockParser.load(type: Province.self, fileName: "NoProducers")
+                expect(noProducers).toNot(beNil())
             }
-            // test에서 기대되는 결과. 위에서 명사로 작성한 테스트 대상의 행동을 작성
+
             it("shortfall") {
-                expect(noProducers?.shortfall).to(equal(30))
+                expect(noProducers.shortfall).to(equal(30))
             }
             
             it("profit") {
-                expect(noProducers?.profit).to(equal(0))
+                expect(noProducers.profit).to(equal(0))
             }
         }
     }
